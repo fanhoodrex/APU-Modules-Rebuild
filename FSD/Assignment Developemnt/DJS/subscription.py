2a387fddb661012408538332c9804577e32fbf17
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2021-03-10 13:58:19 +0800

    Done with Insurance Plan Subscription
    Finish the initial module of Insurance Plan Subscription, but it has't been fully test and might be flawed.

--- FSD/Assignment Developemnt/DJS/subscription.py
+++ FSD/Assignment Developemnt/DJS/subscription.py
@@ -52,3 +52,46 @@ while True:
         time.sleep(2)
         continue
     break
+
+while True:
+    limit_type = input("What type of claim limit type do u want? Either Annual or Lifetime\n:") # try/except clause might needed to be used in user input 
+    if limit_type == "Annual":
+        print("Annual Claim Limit Subscription")
+    elif limit_type == "Lifetime":
+        print("Lifetime Claim Limit Subscription")
+    else:
+        print("Pls enter valid type of either Annual or Lifetime.")
+        time.sleep(2)
+        continue
+    break
+
+def subscriber_details():
+    """input for subscriber details and write into txt file"""
+    return None
+
+def Subscription():
+    id = 1 # global scope
+    while True:
+        name = input("What is your name?:")
+        contact = input("What is your contact number?:")
+        address = input("where is your address?:")
+        HealthHistory = input("What is your health history?")
+        id = id + 1
+
+        print(f"subscriber's id is {id}")
+        print(f"subscriber's name is {name}")
+        print(f"subscriber's contact number is {contact}")
+        print(f"subscriber's address is {address}")
+        print(f"subscriber's health history is {HealthHistory}")
+
+        with open("subscriber.txt",'a',encoding = 'utf-8') as f: # with-open clause to data into plain txt file
+            f.write(f"The subscriber's name is {name}\n")
+            f.write(f"The subscriber's contact number is {contact}\n")
+            f.write(f"The subscriber's address is {address}\n")
+            f.write(f"The subscriber's health history is {HealthHistory}\n")
+        
+        if input("Enter next subscriber details?: Type 1 for Yes & Other key for No") == 1:
+            continue
+        else:
+            break
+    return None
