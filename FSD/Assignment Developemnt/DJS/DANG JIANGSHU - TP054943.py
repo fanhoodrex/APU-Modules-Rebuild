c38bca2d64acb612404e712bcd4945e2cdac8d4b
Fang <Lucas@FZW.local>
2021-03-13 17:52:53 +0800

    Merge into one py file

--- FSD/Assignment Developemnt/DJS/DANG JIANGSHU - TP054943.py
+++ FSD/Assignment Developemnt/DJS/DANG JIANGSHU - TP054943.py
@@ -1,3 +1,6 @@
+# DANG JIANSHU
+# TP054943
+
 import time
 id_list = []
 name_list = []
@@ -122,3 +125,32 @@ def search():
         print("Sorry, the subscriber with this id/name doesn't exist.\n")
         time.sleep(1)
     return None
+
+while True:
+    print("""
+    "Welcome tp MEDILIFE HELATH INSURANCE SCHEME"
+    1. Insurance Plan Subscription
+    2. Claim Processing
+    3. Accounts Information
+    4. Searching Functionalities
+    """)
+    choice = int(input("Pls select (1-4),0 for quit:"))
+    if choice == 1:
+        Subscription()
+        print("\n")
+        print(id_list)
+        print(name_list)
+        print(subscribers)
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        search()
+        continue
+    elif choice == 0:
+        print("Quit successfully")
+        break
+    else:
+        print("Invalid input, pls type in integer within 1-4")
+        continue
