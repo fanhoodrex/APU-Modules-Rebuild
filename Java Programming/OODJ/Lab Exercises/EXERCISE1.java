9574153f7b1ee41bbe67f671840125bbf6555315
Fang <Zachary_Fang@outlook.com>
2021-11-29 16:50:04 +0800

    Rename filename of exercise1
    Consistency between filename and class name

--- Java Programming/OODJ/Lab Exercises/EXERCISE1.java
+++ Java Programming/OODJ/Lab Exercises/EXERCISE1.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-/*
-1.Write a program that reads a Fahrenheit degree in double, then converts it to Celsius and displays the result on the console. The formula for the conversion is as follows: 
-celsius = Fahrenheit – 32 * 5 / 9
-*/
-public class Lab_Exercises {
+public class EXERCISE1 {
+    /*
+    1.Write a program that reads a Fahrenheit degree in double, then converts it to Celsius and displays the result on the console. The formula for the conversion is as follows: 
+    celsius = Fahrenheit – 32 * 5 / 9
+    */
     public static void main(String[] args) {
         Scanner readObject = new Scanner(System.in);
         System.out.print("Please enter the Fahrenheit value:");
