8546be0fa1a2869b4e83fec468757e4fd71fd03b
Fang <Zachary_Fang@outlook.com>
2021-11-30 17:09:14 +0800

    Create EXERCISE5.java

---
+++ Java Programming/OODJ/Lab Exercises/EXERCISE5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EXERCISE5 {
+    /*
+    5.Write a program that receives an ASCII code (an integer between 0 and 128) and displays its character.
+    For example, if the user enters 97, the program displays character ‘a’.
+    */
+    public static void main(String[] args) {
+        Scanner readObject = new Scanner(System.in);
+        System.out.print("Please enter an integer between 0 and 128:");
+        int num = readObject.nextInt();
+        char ASCII = (char) num;
+        System.out.println("The ASCII code for " + num + " is " + ASCII);
+    }
+}
