7312a020c349f910fe18008158cf3592277221b2
Fang <40188521+fanhoodrex@users.noreply.github.com>
2021-11-30 12:04:32 +0800

    Update EXERCISE3.java
    use while loop to iterate and sum up the digits

--- Java Programming/OODJ/Lab Exercises/EXERCISE3.java
+++ Java Programming/OODJ/Lab Exercises/EXERCISE3.java
@@ -4,21 +4,17 @@ public class EXERCISE3 {
     /*
     3.Write a program that reads an integer between 0 and 1000 and adds all the digits in the integer.
     For example, if an integer is 943, the sum of all its digit is 16.
-
-    use loop logic below to complete the exercise3 
-
-    sum = 0
-    num = 567
-    while num > 0
-        id = sum % 10
-        sum += id
-        num = num / 10
     */
     public static void main(String[] args) {
+        int sum = 0;
         Scanner readObject = new Scanner(System.in);
-        System.out.print("Please enter the Fahrenheit value:");
-        double Fahrenheit = readObject.nextDouble();
-        double Celsius = Fahrenheit - 32 * 5 / 9;
-        System.out.println("The Celsius value is " + Celsius);
+        System.out.print("Please enter the integer between 0 and 1000:");
+        int num = readObject.nextInt();
+        while (num > 0){ // while loop to iterate and sum up the digits
+            int digit = sum % 10;
+            sum += digit;
+            num = num / 10;
+        }
+        System.out.println("the sum of all its digit is " + sum);
     }
 }
