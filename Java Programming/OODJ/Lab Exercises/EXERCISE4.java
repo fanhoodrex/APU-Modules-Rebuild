a9ad36af32f0ca03726dddc25d9e3187d5bcfd8a
Fang <40188521+fanhoodrex@users.noreply.github.com>
2021-11-30 12:29:04 +0800

    Create EXERCISE4.java

---
+++ Java Programming/OODJ/Lab Exercises/EXERCISE4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EXERCISE4 {
+    // 4.Write a program that converts an uppercase letter to a lowercase letter.
+    public static void main(String[] args) {
+        Scanner readObject = new Scanner(System.in);
+        System.out.print("Please enter the uppercase letter:");
+        String letter = readObject.nextLine();
+        System.out.println("The lowercase letter: " + letter.toLowerCase());
+    }
+}
