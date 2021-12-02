80f885f519549b2af87e4986f535cba6d6b8951a
Fang <Zachary_Fang@outlook.com>
2021-12-02 12:42:22 +0800

    Add comment to 09

--- OODJ/Lab Exercises/Exercise09.java
+++ OODJ/Lab Exercises/Exercise09.java
@@ -1,4 +1,8 @@
 public class EXERCISE9 {
+    /*
+    9. Suppose that the tuition of a university is RM10000 this year and this tuition fee increases 5% every year.
+    Write a program that uses a loop to compute the tuition in ten years.
+    */
     public static void main(String[] args) {
         float fee = 10000;
         for (int i = 1; i <= 10; i++) {
