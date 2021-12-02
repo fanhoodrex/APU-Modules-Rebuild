9e2feb6e35871cc75d2e89900ffea3ba4c898c83
Fang <Zachary_Fang@outlook.com>
2021-12-02 11:39:24 +0800

    Add Exercise9 answer

---
+++ OODJ/Lab Exercises/Exercise9.java
@@ -0,0 +1,9 @@
+public class Exercise9 {
+    public static void main(String[] args) {
+        float fee = 10000;
+        for (int i = 1; i <= 10; i++) {
+            fee *= 1.05;
+        }
+    System.out.println("Fees after 10 years would be " + Math.round(fee));
+    }
+}
