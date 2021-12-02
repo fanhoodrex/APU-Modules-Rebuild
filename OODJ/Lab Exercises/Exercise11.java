926aef299bbaa572b706c2f4ace739ab675a046d
Fang <Zachary_Fang@outlook.com>
2021-12-02 12:31:55 +0800

    Add 11 answer

--- OODJ/Lab Exercises/Exercise11.java
+++ OODJ/Lab Exercises/Exercise11.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Exercise11 {
     /*
     11.	Write a program that reads in investment amount, annual interest rate, and number of years, and displays the future investment value using the following formula.
-    futureInvestmentVal = investmentAmount x (1 + monthlyInterestRate) numberOfYears*12
+    futureInvestmentVal = investmentAmount x (1 + monthlyInterestRate) x numberOfYears*12
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String ans = "Yes";
-        do {
-            System.out.print("Do u want to continue (Yes/No):");
-            ans = input.nextLine();
-        } while (ans.equals("Yes"));
+
+        System.out.print("Enter investment amount: ");
+        double investmentAmount = input.nextFloat();
+        System.out.print("Enter annual interest rate: ");
+        double annualInterestRate = input.nextFloat();
+        System.out.print("Enter number of years: ");
+        int numberOfYears = input.nextInt();
+
+        double futureInvestmentVal = investmentAmount * Math.pow(1 + annualInterestRate / 12, numberOfYears);
+        futureInvestmentVal = Math.round(futureInvestmentVal * 100) / 100;
+
+        System.out.print("Future investment value is: " + futureInvestmentVal);
     }
 }
