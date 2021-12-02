6ff447c966c2437d41d5efac9b769ad7e4e7895f
Fang <Zachary_Fang@outlook.com>
2021-12-02 12:00:48 +0800

    Create Exercise10.java

---
+++ OODJ/Lab Exercises/Exercise10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise10 {
+    /*
+    10.Use do-while construct, write a program that prompts the users to continue the program execution.
+    “Yes” to continue the program and “No” to terminate the program.
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String ans = "Yes";
+        do {
+            System.out.print("Do u want to continue (Yes/No):");
+            ans = input.nextLine();
+        } while (ans.equals("Yes"));
+    }
+}
