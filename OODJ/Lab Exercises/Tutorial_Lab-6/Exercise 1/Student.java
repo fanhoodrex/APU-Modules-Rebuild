7e8871e87154344f5d0ecafa9bc7813c24b06c6e
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2022-03-08 11:07:33 +0800

    Update Student.java

--- OODJ/Lab Exercises/Tutorial_Lab-6/Exercise 1/Student.java
+++ OODJ/Lab Exercises/Tutorial_Lab-6/Exercise 1/Student.java
@@ -2,9 +2,18 @@
  * Student
  */
 public class Student extends Person {
-    private final Status status;
-    
-    public Student(Status status){
+    private String status;
+
+    public Student(String name, String address, int PhoneNo, String email, String status){
+        super(name, address, PhoneNo, email, officename, salaryamount, datehired);
         this.status = status;
+    }
+
+    public enum Status {
+        FRESHMAN, SOPHOMORE, JUNIOR, SENIOR;
+    } 
+    
+    public String toString(){
+        return "Class:Student \nName: " + name;
     }
 }
